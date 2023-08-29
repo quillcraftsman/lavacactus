fe24a3fb57886680c107efa7dcefa10fe84142c6
S1wH <gomozov.pavel@yandex.ru>
2023-08-29 12:35:28 +0300

    new method pkg_walk_2() created now .github/workflows appears in project skeleton few changes in bootstrap_from_package() method in writing files

--- cactus/bootstrap/package.py
+++ cactus/bootstrap/package.py
@@ -2,18 +2,16 @@
 import os
 import posixpath #TODO: Windows?
 import logging
-import pkg_resources
 
-from cactus.utils.packaging import pkg_walk
+from cactus.utils.packaging import pkg_walk_2
 
 
 logger = logging.getLogger(__name__)
 
 
 def bootstrap_from_package(path):
-    for dir_, sub_dirs, filenames in pkg_walk("cactus", "skeleton"):
+    for dir_, sub_dirs, filenames in pkg_walk_2("cactus", "skeleton"):
         base_path = os.path.join(path, dir_.split('skeleton', 1)[1].lstrip('/'))
-
         for sub_dir in sub_dirs:
             dir_path = os.path.join(base_path, sub_dir)
             logger.debug("Creating {0}".format(dir_path))
@@ -24,5 +22,6 @@ def bootstrap_from_package(path):
             file_path = os.path.join(base_path, filename)
 
             logger.debug("Copying {0} to {1}".format(resource_path, file_path))
-            with open(file_path, 'wb') as f:
-                f.write(pkg_resources.resource_stream("cactus", resource_path).read())
+            with open(file_path, 'wb') as f_writer:
+                with open(os.path.join("cactus", resource_path), "rb") as f_reader:
+                    f_writer.write(f_reader.read())
--- cactus/utils/packaging.py
+++ cactus/utils/packaging.py
@@ -1,14 +1,13 @@
 import posixpath
 import pkg_resources
+import os
 
 
 def pkg_walk(package, top):
     """
     Walk the package resources. Implementation from os.walk.
     """
-
     names = pkg_resources.resource_listdir(package, top)
-
     dirs, nondirs = [], []
 
     for name in names:
@@ -17,10 +16,21 @@ def pkg_walk(package, top):
             dirs.append(name)
         else:
             nondirs.append(name)
-
     yield top, dirs, nondirs
 
     for name in dirs:
         new_path = posixpath.join(top, name)
         for out in pkg_walk(package, new_path):
+            yield out
+
+
+def pkg_walk_2(package, top):
+    # we get names tuple names where names[0] is current folder, names[1] is list of dirs and names[2] is list of files
+    names = os.walk(os.path.join(package, top)).__next__()
+    dirs, nondirs = [dir for dir in names[1]], [nondir for nondir in names[2]]
+    yield top, dirs, nondirs
+    # loop through dirs to do the same thing as we did before
+    for name in dirs:
+        new_path = posixpath.join(top, name)
+        for out in pkg_walk_2(package, new_path):
             yield out
